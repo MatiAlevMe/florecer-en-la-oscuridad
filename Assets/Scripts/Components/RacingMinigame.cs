50f4ea239a1c93204c02fc7dba1f7fb7e8cb5971
Arakito14 <inefableataraxia1@gmail.com>
2025-03-02 02:47:22 -0300

    feat: Add jump, race start, win condition to racing minigame

--- Assets/Scripts/Components/RacingMinigame.cs
+++ Assets/Scripts/Components/RacingMinigame.cs
@@ -1,16 +1,56 @@
-using UnityEngine;
-
-namespace Components
-{
-    public class RacingMinigame : MonoBehaviour
-    {
-        public Transform playerCar;
-        public float speed = 10f;
-
-        void Update()
-        {
-            float horizontal = Input.GetAxis("Horizontal");
-            playerCar.position += new Vector3(horizontal, 0, 0) * speed * Time.deltaTime;
-        }
-    }
-}
+                                                                                                                                                        
+ using UnityEngine;                                                                                                                                      
+                                                                                                                                                         
+ namespace Components                                                                                                                                    
+ {                                                                                                                                                       
+     public class RacingMinigame : MonoBehaviour                                                                                                         
+     {                                                                                                                                                   
+         public Transform playerCar;                                                                                                                     
+         public float speed = 10f;                                                                                                                       
+         public float jumpForce = 5f;                                                                                                                    
+         public Transform finishLine;                                                                                                                    
+         private bool isRacing = false;                                                                                                                  
+                                                                                                                                                         
+         void Update()                                                                                                                                   
+         {                                                                                                                                               
+             if (isRacing)                                                                                                                               
+             {                                                                                                                                           
+                 float horizontal = Input.GetAxis("Horizontal");                                                                                         
+                 playerCar.position += new Vector3(horizontal, 0, 0) * speed * Time.deltaTime;                                                           
+                                                                                                                                                         
+                 if (Input.GetButtonDown("Jump") && IsGrounded())                                                                                        
+                 {                                                                                                                                       
+                     playerCar.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);                                    
+                 }                                                                                                                                       
+                                                                                                                                                         
+                 if (IsNearFinishLine())                                                                                                                 
+                 {                                                                                                                                       
+                     WinRace();                                                                                                                          
+                 }                                                                                                                                       
+             }                                                                                                                                           
+         }                                                                                                                                               
+                                                                                                                                                         
+         void StartRace()                                                                                                                                
+         {                                                                                                                                               
+             isRacing = true;                                                                                                                            
+             playerCar.position = new Vector3(-10, 0, 0);                                                                                                
+         }                                                                                                                                               
+                                                                                                                                                         
+         void WinRace()                                                                                                                                  
+         {                                                                                                                                               
+             isRacing = false;                                                                                                                           
+             // Añade lógica para ganar la carrera                                                                                                       
+             Debug.Log("Victoria!");                                                                                                                     
+         }                                                                                                                                               
+                                                                                                                                                         
+         bool IsGrounded()                                                                                                                               
+         {                                                                                                                                               
+             return playerCar.position.y <= 0.1f;                                                                                                        
+         }                                                                                                                                               
+                                                                                                                                                         
+         bool IsNearFinishLine()                                                                                                                         
+         {                                                                                                                                               
+             return Vector3.Distance(playerCar.position, finishLine.position) < 1f;                                                                      
+         }                                                                                                                                               
+     }                                                                                                                                                   
+ }   
